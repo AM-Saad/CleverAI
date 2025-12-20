5197039f1f1fbf37569a41e4a24395ec958009ac
AM-Saad <amsstudio.e@gmail.com>
2025-12-20 23:48:12 +0200

    chore(import): disable global auth

--- server/api/debug.config.ts
+++ server/api/debug.config.ts
@@ -1,10 +1,7 @@
 // server/api/debug.config.ts
-export default defineEventHandler(async () => {
-  const users = await prisma.user.findMany({ take: 5 });
-  console.log("Sample users from database:", users);
+export default defineEventHandler(() => {
   return {
     authOrigin: useRuntimeConfig().public.AUTH_ORIGIN,
     appBaseUrl: useRuntimeConfig().public.APP_BASE_URL,
-    sampleUsers: users,
   };
 });
